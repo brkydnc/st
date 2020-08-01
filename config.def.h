8468f5879be821dc39b97333dbd6fc991a47e2e8
Berkay Dinç <berkayydinc@gmail.com>
2020-08-01 22:29:17 +0300

    Increase font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Roboto Mono for Powerline:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Roboto Mono for Powerline:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
